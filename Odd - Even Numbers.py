6989ea65b31e683068f70ef9dcbe2a432d58c70d
jayceee1207 <ablayjc7@gmail.com>
2023-04-24 19:46:10 +0800

    Pseudocode

--- Odd - Even Numbers.py
+++ Odd - Even Numbers.py
@@ -2,3 +2,19 @@
 #BSCPE 1-5
 #Assignment 4: Program 1
 #April 22, 2023
+
+#PSEUDOCODE
+#Open a file named "numbers.txt"
+#Read every line of numbers
+#Create a list where we store all the even numbers
+#Create a list where we store all the odd numbers
+#Make a for loop for the number file
+#for number in number file
+#   convert number into an integer
+#   if number is even
+#       append that number into the even numbers list
+#   if number is odd
+#       append that number into the odd numbers list
+#open a file to store all numbers in even numbers list
+#open a file to store all numbers in odd numbers list
+
