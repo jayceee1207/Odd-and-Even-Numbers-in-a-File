1f159ca312287b0773d0a71a146008d7b706a2a0
jayceee1207 <ablayjc7@gmail.com>
2023-04-25 11:14:22 +0800

    Craete a list for even and odd numbers

--- Odd - Even Numbers.py
+++ Odd - Even Numbers.py
@@ -12,10 +12,12 @@ file_open = open("numbers.txt", "r")
 #Read every line of numbers
 read_content = file_open.readlines()
 #Create a list where we store all the even numbers
+even_nums = []
 #Create a list where we store all the odd numbers
+odd_nums = []
 #Make a for loop for the number file
 #for number in number file
-#   convert number into an integer
+#   convert nu mber into an integer
 #   if number is even
 #       append that number into the even numbers list
 #   if number is odd
